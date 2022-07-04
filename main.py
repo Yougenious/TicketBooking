084ea92a7555d891e379393ff47c108b4585b2f4
Yougenious <108633621+Yougenious@users.noreply.github.com>
2022-07-04 20:03:08 +0300

    Second commit
    Mistakes corrected, functions added(start_seats, read_seats)

--- main.py
+++ main.py
@@ -1,44 +1,80 @@
 from tkinter import *
 from json import *
-root = Tk()
-root.geometry('800x1000')
+
+
+def start_seats():
+    '''
+    Creating new file of avaliable seats, if such file doesn't exists.
+    '''
+    # Hashing every seat with code with number of row and column
+    places = {}
+    for i in range(len(seats)):
+        for j in range(len(seats[i])):
+            places[hasher(i, j)] = True
+    with open('seats.json', 'w') as file:
+        dump(places, file)
+
+
+def read_seats():
+    with open("seats.json", "r") as file:
+        return load(file)
+
 
 def bookseat(event):
+    '''
+    Making every free seat booked. Controls every button by means of event.
+    If current seat is free, make it red and booked.
+    Show label about current booked seat.
+    :param event:
+    :return:
+    '''
     if event.widget.free:
         event.widget.config(bg='red')
         Label(root, text=f'{event.widget.row} {event.widget.place} is booked').place(x=10, y=850)
 
+
 def hasher(row, place):
-    return 100*row+place
+    '''
+    Creating unique code for every seat
+    :param row: row
+    :param place: column
+    :return: code as integer
+    '''
+    return 100 * row + place
+
 
 def freeseats():
-    # for i in range(len(seats)):
-    #     for j in range(len(seats[i])):
-            seats[hasher(i, j)] = True
-            if not seats[i][j].free:
-                with open(freeseats.json, 'w') as file:
-                    dump(seats, file)
+    seats[hasher(i, j)] = True
+    if not seats[i][j].free:
+        with open(freeseats.json, 'w') as file:
+            dump(seats, file)
 
-
-seats = {}
-seats = [[Button(root, text=i, width =4, height =2, bg = 'green') for i in range (1, 16)] for g in range(20)]
+# Creating main window
+root = Tk()
+root.geometry('800x1000')
+# Creating matrix of seats
+seats = [[Button(root, text=i, width=4, height=2, bg='green') for i in range(1, 16)] for g in range(20)]
+# Printing matrix of seats to the window
 for i in range(len(seats)):
     for j in range(len(seats[i])):
         seats[i][j].free = True
-        seats[i][j].price = 3000-100*i
-        seats[i][j].row = i+1
-        seats[i][j].place = j+1
+        seats[i][j].price = 3000 - 100 * i
+        seats[i][j].row = i + 1
+        seats[i][j].place = j + 1
         seats[i][j].grid(row=i, column=j)
         seats[i][j].bind('<Button-1>', bookseat)
+# Printing prices fo all rows
 for i in range(len(seats)):
-    Label(root, text=i+1).grid(row=i, column=16)
-    Label(root, text=3000-100*i).grid(row=i, column=17)
+    Label(root, text=i + 1).grid(row=i, column=16)
+    Label(root, text=3000 - 100 * i).grid(row=i, column=17)
 
-places = {}
-for i in range(len(seats)):
-    for j in range(len(seats[i])):
-        places[hasher(i, j)] = True
-with open('seats.json', 'w') as file:
-    dump(places, file)
+try:
+    places = read_seats()
+except FileNotFoundError:
+    start_seats()
+except:
+    print("Something is going wrong!")
+
+print(read_seats())
 
 root.mainloop()
